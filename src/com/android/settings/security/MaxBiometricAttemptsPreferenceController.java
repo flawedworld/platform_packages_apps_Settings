859fe18e1a3afe77a3af7f28f720e00249c15a84
flawedworld <flawedworld@flawed.world>
2021-08-28 13:41:37 +0000

    Settings for max biometric attempts

---
+++ src/com/android/settings/security/MaxBiometricAttemptsPreferenceController.java
@@ -0,0 +1,82 @@
+package com.android.settings.security;
+
+import android.content.Context;
+import android.os.UserManager;
+import android.provider.Settings;
+import android.util.Log;
+
+import androidx.preference.ListPreference;
+import androidx.preference.Preference;
+import androidx.preference.PreferenceCategory;
+import androidx.preference.PreferenceScreen;
+
+import com.android.settings.core.PreferenceControllerMixin;
+import com.android.settingslib.core.AbstractPreferenceController;
+import com.android.settingslib.core.lifecycle.events.OnResume;
+
+public class MaxBiometricAttemptsPreferenceController extends AbstractPreferenceController
+    implements PreferenceControllerMixin, OnResume,
+           Preference.OnPreferenceChangeListener {
+
+    private static final String KEY_MAX_BIOMETRIC_ATTEMPTS = "max_biometric_attempts";
+    private static final String PREF_KEY_SECURITY_CATEGORY = "security_category";
+
+    private PreferenceCategory mSecurityCategory;
+    private boolean mIsAdmin;
+    private final UserManager mUm;
+
+    public MaxBiometricAttemptsPreferenceController(Context context) {
+        super(context);
+        mUm = UserManager.get(context);
+    }
+
+    @Override
+    public void displayPreference(PreferenceScreen screen) {
+        super.displayPreference(screen);
+        mSecurityCategory = screen.findPreference(PREF_KEY_SECURITY_CATEGORY);
+        updatePreferenceState();
+    }
+
+    @Override
+    public boolean isAvailable() {
+        mIsAdmin = mUm.isAdminUser();
+        return mIsAdmin;
+    }
+
+    @Override
+    public String getPreferenceKey() {
+        return KEY_MAX_BIOMETRIC_ATTEMPTS;
+    }
+
+    // TODO: should we use onCreatePreferences() instead?
+    private void updatePreferenceState() {
+        if (mSecurityCategory == null) {
+            return;
+        }
+
+        if (mIsAdmin) {
+            ListPreference maxBiometricAttempts =
+                    (ListPreference) mSecurityCategory.findPreference(KEY_MAX_BIOMETRIC_ATTEMPTS);
+            maxBiometricAttempts.setValue(Long.toString(Settings.Global.getLong(
+                    mContext.getContentResolver(), Settings.Global.MAX_BIOMETRIC_ATTEMPTS, 0)));
+        } else {
+            mSecurityCategory.removePreference(
+                    mSecurityCategory.findPreference(KEY_MAX_BIOMETRIC_ATTEMPTS));
+        }
+    }
+
+    @Override
+    public void onResume() {
+        updatePreferenceState();
+    }
+
+    @Override
+    public boolean onPreferenceChange(Preference preference, Object value) {
+        final String key = preference.getKey();
+        if (KEY_MAX_BIOMETRIC_ATTEMPTS.equals(key) && mIsAdmin) {
+            long timeout = Long.parseLong((String) value);
+            Settings.Global.putLong(mContext.getContentResolver(), Settings.Global.MAX_BIOMETRIC_ATTEMPTS, timeout);
+        }
+        return true;
+    }
+}
